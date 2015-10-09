311ee51f4065a895e1ebf5758d0a092d255288d1
Taylor Hamling <taylor@taylorhamling.com>
2015-10-09 17:16:26 +1300

    latest

--- index.php
+++ index.php
@@ -1,9 +1,8 @@
 <?php
 
 require_once("odooconnector.class.php");
-
 $odoo = new OdooConnector("http://test.triotech.co.nz:8069", "flowers");
-//$odoo->print_json(true,$odoo->login("flowers","admin","admin")); //login to odoo
+
 try{
 	if ($_SERVER['REQUEST_METHOD'] == 'POST' && empty($_POST))
 		$_POST = (array) json_decode(file_get_contents('php://input'), true);
@@ -15,18 +14,38 @@ try{
 	}
 		
 	//login GET username, password
+	else if (isset($_GET["loginportal"]) && isset($_GET["username"]) && isset($_GET["password"])){
+		$odoo->print_json(true,$odoo->loginPortal($_GET["username"],$_GET["password"]));
+            //$odoo->print_json(true,$odoo->login("flowers","admin","admin"));
+	}
+        
+	//loginportal GET username, password
 	else if (isset($_GET["login"]) && isset($_GET["username"]) && isset($_GET["password"])){
 		$odoo->print_json(true,$odoo->login($_GET["username"],$_GET["password"]));
             //$odoo->print_json(true,$odoo->login("flowers","admin","admin"));
-	}
+	}        
 	//logout GET sessionid
 	else if (isset($_GET["logout"]) && isset($_GET["sessionid"])){
-		$odoo->setSession($_GET["logout"]);
+		$odoo->setSession($_GET["sessionid"]);
 		$odoo->print_json(true,$odoo->logout(true));
 	}
+        //register
+	else if (isset($_POST["register"]) && isset($_POST["username"]) && isset($_POST["password"]) && isset($_POST["passwordconfirm"])){
+            $odoo->print_json(true,$odoo->register($_POST));
+	}    
+        //reset password
+        else if (isset($_POST["resetpassword"]) && isset($_POST["email"])){
+            $odoo->print_json(true,$odoo->resetPassword($_POST["email"]));
+        }
+        //update user
+	else if (isset($_POST["updateuser"]) && isset($_POST["sessionid"])){
+            $odoo->setSession($_POST["sessionid"]);
+            $odoo->print_json(true,$odoo->updateUser($_POST));
+	}         
+        
 	//get user info GET sessionid
 	else if (isset($_GET["user"]) && isset($_GET["sessionid"])){
-		$odoo->setSession($_GET["logout"]);
+		$odoo->setSession($_GET["sessionid"]);
 		$odoo->print_json(true,$odoo->getSessionInfo());
 	}
 
@@ -42,26 +61,31 @@ try{
 		if ($_GET["model"] === "product.template"){
 			$search = array(array("uom_id", "=", 1));
 		}
+                
+                elseif (isset($_GET["partnerid"])){
+                    $search = array(array("partner_id", "=", (int) $_GET["partnerid"]));
+                }
 		$count = $odoo->call($_GET["model"],'search_count',array($search));
 		$data = $odoo->call($_GET["model"],"search_read",array($search),array("offset"=>$offset,"limit"=>$limit,"order"=>$order));
+                foreach($data["result"] as $key => $item){ //get local user info                
+                    if (count($item["partner_id"]) > 0){
+                        $data["result"][$key]["user_data"] = $odoo->getLocalUser($item["partner_id"][0]);
+                    }
+                }
 		$odoo->print_json(true,array("count"=>$count["result"],"data"=>$data["result"]));
 	}
-
+        //GET SINGLE/SPECIFIC ID
+	else if (isset($_POST["records"]) && isset($_POST["single"]) && isset($_POST["model"]) && isset($_POST["sessionid"])){
+		$odoo->setSession($_POST["sessionid"]);		
+		$data = $odoo->call($_POST["model"],"read",$_POST["single"]);
+		$odoo->print_json(true,$data);
+	}	
+        
 	else if (isset($_POST["report"]) && isset($_POST["model"]) && isset($_POST["sessionid"]) && isset($_POST["args"])){
 		$odoo->setSession($_GET["sessionid"]);
 
 		$data = $odoo->call($_GET["model"],"search",array($_POST["args"]) );
 		$odoo->print_json(true,array("count"=>$count["result"],"data"=>$data["result"]));
-	}
-	
-	//POST SEARCH - WITH ARGUMENTS (REQUIRES POSTING JSON)
-	else if (isset($_POST["records"]) && isset($_POST["search"]) && isset($_POST["model"]) && isset($_POST["args"]) && isset($_POST["sessionid"])){
-		$odoo->setSession($_POST["sessionid"]);
-		$kwargs = array();
-		if (isset($_POST["kwargs"])){
-			$kwargs = json_decode($_POST["kwargs"]);
-		}
-		$odoo->print_json(true,$odoo->call($_POST["model"],"search_read",json_decode($_POST["args"]),$kwargs));
 	}	
 	
 	//POST
@@ -76,10 +100,10 @@ try{
             $odoo->setSession($_POST["sessionid"]);
             $odoo->print_json(true,$odoo->call($_POST["model"],"write",array($_POST["args"]["ids"], $_POST["args"]["data"]), $_POST["kwargs"]));
 	}
-	//PUT STATE
+	//PUT STATE/STATUS
 	else if (isset($_POST["records"]) && isset($_POST["state"]) && isset($_POST["model"]) && isset($_POST["args"]) && isset($_POST["sessionid"])){
             $odoo->setSession($_POST["sessionid"]);
-            $odoo->print_json(true,$odoo->call($_POST["model"],"action_" . $_POST["state"],array($_POST["args"]["id"])));		
+            $odoo->print_json(true,$odoo->callAction($_POST["model"],"action_" . $_POST["state"],array($_POST["args"]["id"])));		
 	}
 	
 	//DELETE
@@ -114,31 +138,10 @@ try{
 	else{
 		throw new Exception('No end point found with that request type!');
 	}
-	
 
-	//create new customer POST
-	//update customer PUT
-	//delete customer DELETE
-	//add order POST
-	//update order PUT
-	//delete order DELETE
-	//add stock POST
-	//update stock PUT
-	//delete stock DELETE
-
-
-
-
-	
-
-	//print_r($odoo->getSessionInfo()); //get current session info, returns false if no session exists
-
-	//print_r($odoo->call("res.partner","search",array(array(array('is_company', '=', true),array('customer', '=', true))))); //search customers
-				  
-	//print_r($odoo->call('res.partner', 'create',array(array('name'=>"New Partner from api")))); //create new customer	  
 
 }catch(Exception $e){
-	return $odoo->print_json(false,$e);
+	return $odoo->print_json(false,$e->getMessage());
 }
 
 
--- odooconnector.class.php
+++ odooconnector.class.php
@@ -1,17 +1,115 @@
 <?php
+require  'database/medoo.php';
 
 class OdooConnector { 
 	private $baseUrl;
 	private $db; 
 	private $sessionId;
 	private $context;
+	private $localDb;
 	
 	public function __construct ( $baseUrl, $db) {
 		$this->baseUrl = $baseUrl;
 		$this->db = $db; //safer/easier to pass this as a constructor and hardcode in php
+		$this->localDb = new medoo(array('database_type' => 'sqlite','database_file' => 'database/users.db')); //required pdo php extension
 	  }	
-    
-	public function login($login,$password){
+          
+	public function register($data){
+		if ($data["password"] !== $data["passwordconfirm"]){ //check passwords match
+			throw new Exception("Passwords do not match.");
+		}
+		$login = $this->login("admin","admin"); //login to odoo
+
+		if (!isset($login["result"]["session_id"])){ //check odoo login worked
+			throw new Exception("Registration failed. Invalid Odoo authentication.");
+		}
+		$sessionid = $login["result"]["session_id"];
+		$this->setSession($sessionid);
+		if ($data["type"] === "grower" || $data["type"] === "picker"){ //if user type is grower or picker, set as supplier in odoo
+			$result = $this->call("res.partner","create",array(array("name"=>$data["username"], "supplier"=>true, "email"=>$data["email"])));
+		}
+		else{$result = $this->call("res.partner","create",array(array("name"=>$data["username"], "customer"=>true, "email"=>$data["email"])));} //otherwise set at customer
+		$partnerid = $result["result"];
+		$password = password_hash($data["password"], PASSWORD_DEFAULT);
+	   
+		$localid = $this->localDb->insert("users", array("username"=>$data["username"], "password"=>$password, "partnerid"=>$partnerid, "type"=>$data["type"])); //insert into local database
+		return array("user"=>array("username"=>$data["username"], "partnerid"=>$partnerid, "session_id"=>$sessionid, "local_userid"=>$localid, "type"=>$data["type"]), "partner"=>array("id"=>$partnerid));
+	}  
+	
+	public function updateUser($data){
+		if (isset($data["password"]) && isset($data["currentpassword"]) && ($data["password"] === $data["passwordconfirm"])){ //password change - local db only
+			$result = $this->localDb->select("users", "*", ["id"=>$data["userid"]]); 
+
+			if (count($result) < 1){ //make sure this user actually exists 
+				throw new Exception("No user with this id.");
+			}
+			$user = $result[0];
+
+			if (!password_verify($data["currentpassword"], $user["password"])){ //check they entered their current password correctly
+				throw new Exception("Current password invalid.");
+
+			};                
+			$password = password_hash($data["password"], PASSWORD_DEFAULT); //hash password
+			$this->localDb->update("users", array("password"=>$password), array("id"=>$data["userid"])); //update it
+		}
+		
+		
+		if (isset($data["type"])){ //user type change - local db and on odoo
+			$this->localDb->update("users", array("type"=>$data["type"]), array("id"=>$data["userid"])); //change type locally
+			if ($data["type"] === "buyer"){ //if changed to buyer, update odoo record as customer
+				$result = $this->call("res.partner","write",array(array((int) $data["partnerid"]), array('supplier'=>false, 'customer'=>true)), array("context"=>array("params"=>array("action"=>61), "uid"=>1)));
+			}
+			else if ($data["type"] === "grower" || $data["type"] === "picker"){ //if changed to grower or picker, update odoo record as supplier
+				$this->call("res.partner","write",array(array((int) $data["partnerid"]),array('supplier'=>true, 'customer'=>false)), array("context"=>array("params"=>array("action"=>61), "uid"=>1)));
+			}
+
+		}
+		
+		if (isset($data["email"])){ //email + other field changes - only on odoo
+			$this->call("res.partner","write",array(array((int) $data["partnerid"]),array('email'=>$data['email'])),array("context"=>array("params"=>array("action"=>61), "uid"=>1)));
+		}
+		return array("user"=>array("partnerid"=>$data["partnerid"], "local_userid"=>$data["userid"], "type"=>$data["type"]));
+		
+	}  
+        
+        public function getLocalUser($id){
+            $result = $this->localDb->select("users", ["username", "id","partnerid"], ["partnerid"=>$id]); 
+            if (count($result) < 1){ //make sure this user actually exists 
+                    return array();
+            }
+            return $result[0];            
+        }
+        
+	public function loginPortal($username, $password){
+		$login = $this->login("admin","admin"); //login in odoo
+		
+		if (!isset($login["result"]["session_id"])){ //check it worked
+			throw new Exception("Registration failed. Invalid Odoo authentication.");
+		}
+		$sessionid = $login["result"]["session_id"];
+		$this->setSession($sessionid);   
+		
+		$result = $this->localDb->select("users", "*", ["username"=>$username]);
+		
+		if (count($result) < 1){ //check user exists
+			throw new Exception("No user with this username.");
+		}
+		$user = $result[0];
+
+		if (!password_verify($password, $user["password"])){ //verify password
+			throw new Exception("Invalid password.");
+			
+		};
+		
+		$partner = $this->call("res.partner","search_read",array(array(array("id", "=", $user["partnerid"])))); //get odoo data for user
+		
+		return array("user"=>array("session_id"=>$sessionid,"username"=>$username, "local_userid"=>$user["id"], "partnerid"=>$user["partnerid"], "type"=>$user["type"]), "partner"=>$partner["result"][0]);
+
+		
+		
+	}
+        
+	public function login($login,$password){ //odoo login
 		$result = $this->sendRequest('/web/session/authenticate',array("db"=>$this->db,"login"=>$login,"password"=>$password));
 		if (isset($result["result"]["uid"])){
 			$this->context = $result["result"]["user_context"];
@@ -23,37 +121,79 @@ class OdooConnector {
 		}
 	}
 	
-	public function isLoggedIn($force){ 
+	public function isLoggedIn($force){
 		if (!$force){
-			return isset($this->sessionId);
+			return isset($this->sessionId); //check if the current object has a session
 		}
-		$result = $this->getSessionInfo();
+		$result = $this->getSessionInfo(); //check odoo as well
 		return isset($result["uid"]);
 	}	
 	
 	public function logout($force) {
-		unset($this->session_id);
+		unset($this->session_id); //destroy local session store
 		if ($force){
 			$result = $this->getSessionInfo();
 			if (isset($result["db"])){
-				$this->login($result["db"],"","");
+				$this->login($result["db"],"",""); //destroy it in odoo
 			}
 		}
 	}
+        
+        public function resetPassword($email){
+		$login = $this->login("admin","admin"); //login in odoo
+		
+		if (!isset($login["result"]["session_id"])){ //check it worked
+			throw new Exception("Password reset failed. Invalid Odoo authentication.");
+		}
+		$sessionid = $login["result"]["session_id"];
+		$this->setSession($sessionid);             
+            
+                //find partnerid associated to user in odoo
+                $partner = $this->call("res.partner","search_read",array(array(array("email", "=", $email))));
 
+                if (count($partner["result"]) < 1){
+                    throw new Exception("No user with this email.");
+                }
+                $partnerid = $partner["result"][0]["id"];
+                
+                
+                $result = $this->localDb->select("users", "*", ["partnerid"=>$partnerid]);
+
+                if (count($result) < 1){ //check user exists
+                        throw new Exception("No user assigned to this email in Odoo.");
+                }
+                $user = $result[0];   
+                
+                $newPassword = md5($user["username"] . time());
+                $newPasswordHash = password_hash($newPassword, PASSWORD_DEFAULT); //hash password
+                $this->localDb->update("users", array("password"=>$newPasswordHash), array("id"=>$user["id"])); //update it                
+
+                $subject = "Password Reset - Manawatu Flowers Portal";
+                $email_address = "info@manawatuflowers.com";
+                $message = "You're new password is: " . $newPassword;
+
+                $headers = 'From: ' . $email_address . "\r\n" .
+                   'Reply-To: ' . $email_address . "\r\n" .
+                   'X-Mailer: PHP/' . phpversion();
+
+
+                mail($email, $name, $message, $headers);                
+                return $newPassword;
+                
+        }
 
 	public function getSessionInfo(){
 		if (!isset($this->sessionId)){
 			throw new Exception("No session set.");
 		}
-                $sessionData = $this->sendRequest('/web/session/get_session_info?session_id=' .$this->sessionId, array());
-                $this->context = $sessionData["result"]["user_context"];
+		$sessionData = $this->sendRequest('/web/session/get_session_info?session_id=' .$this->sessionId, array());
+		$this->context = $sessionData["result"]["user_context"];
 		return $sessionData;
 	}	
 	
 	public function setSession($sessionId){
 		$this->sessionId = $sessionId;
-                $this->getSessionInfo();
+		$this->getSessionInfo();
 	}
 	
 	public function getServerInfo(){
@@ -61,46 +201,50 @@ class OdooConnector {
 	}	
 	
 	public function call($model, $method, $args, $kwargs = array()){
-
-		if (!isset($kwargs["context"])){
-                    
+		if (!isset($kwargs["context"])){//if the context hasn't been set manually, set it based on current session
 			$kwargs["context"] = array();
-			array_merge($kwargs["context"], $this->context);
-                        
+			array_merge($kwargs["context"], $this->context);                     
 		}
-                
-                if (!$args){
-                    $args = array();
-                }
-				//$kwargs = array("context"=>array("lang" => "en_US", "tz" => "Pacific/Auckland", "uid"=> 1, "params" => array("action" => 173)));
+		if (!$args){
+			$args = array();
+		}
+		//$kwargs = array("context"=>array("lang" => "en_US", "tz" => "Pacific/Auckland", "uid"=> 1, "params" => array("action" => 173))); //action attribute is important for edits
 				
 		return $this->sendRequest('/web/dataset/call_kw?session_id=' .$this->sessionId, array("model"=>$model,"method"=>$method,"args"=>$args,"kwargs"=>$kwargs));
 		
 	}
-	
-	public function getReport($model, $method, $args, $kwargs = array()){
-            
-		if (!isset($kwargs["context"])){
-                    
+        
+	public function callAction($model, $method, $args, $kwargs = array()){
+		if (!isset($kwargs["context"])){//if the context hasn't been set manually, set it based on current session
 			$kwargs["context"] = array();
-			array_merge($kwargs["context"], $this->context);
-                        
+			array_merge($kwargs["context"], $this->context);                     
 		}
-                
-                if (!$args){
-                    $args = array();
-                }
-		return $this->sendRequest('/web/web_graph/check_xlwt?session_id=' .$this->sessionId, array("model"=>$model,"method"=>$method,"args"=>$args,"kwargs"=>$kwargs));
+		if (!$args){
+			$args = array();
+		}
+		//$kwargs = array("context"=>array("lang" => "en_US", "tz" => "Pacific/Auckland", "uid"=> 1, "params" => array("action" => 173))); //action attribute is important for edits
+				
+		return $this->sendRequest('/web/dataset/call_button?session_id=' .$this->sessionId, array("model"=>$model,"method"=>$method,"args"=>$args));
 		
+	}        
+	
+	public function getReport($model, $method, $args, $kwargs = array()){ //TODO       
+		if (!isset($kwargs["context"])){            
+			$kwargs["context"] = array();
+			array_merge($kwargs["context"], $this->context);                    
+		}             
+		if (!$args){
+			$args = array();
+		}
+		return $this->sendRequest('/web/web_graph/check_xlwt?session_id=' .$this->sessionId, array("model"=>$model,"method"=>$method,"args"=>$args,"kwargs"=>$kwargs));	
 	}	
         
-        function print_json($result, $data){
-                print_r(json_encode(array("result"=>$result,"data"=>$data)));
-        }        
+	public function print_json($result, $data){ //for returning json to client
+			print_r(json_encode(array("result"=>$result,"data"=>$data)));
+	}        
     
-    private function sendRequest($url,$params) { 
+    private function sendRequest($url,$params) { //actually sending stuff to odoo
 		$content = json_encode(array("jsonrpc: '2.0'" => '2.0', "method" => "call", "params" => $params));
-
 		$curl = curl_init($this->baseUrl . $url);
 		curl_setopt($curl, CURLOPT_HEADER, false);
 		curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
@@ -109,14 +253,11 @@ class OdooConnector {
 		curl_setopt($curl, CURLOPT_POSTFIELDS, $content);
 		$json_response = curl_exec($curl);
 		$status = curl_getinfo($curl, CURLINFO_HTTP_CODE);
-
 		if ( $status != 200 ) {
 			throw new Exception("Error: call to URL $url failed with status $status, response $json_response, curl_error " . curl_error($curl) . ", curl_errno " . curl_errno($curl));
 		}
-
 		curl_close($curl);
-		$response = json_decode($json_response, true);
-		
+		$response = json_decode($json_response, true);	
 		return $response;
     } 
 } 
